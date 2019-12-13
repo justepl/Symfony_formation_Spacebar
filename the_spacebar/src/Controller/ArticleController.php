5bc0c64e1610befcbf166b2b41a82ade39f6ec0c
Lamatiere <patrick.lamatiere@gmial.com>
2019-12-13 10:52:17 +0100

    add security-checker

--- the_spacebar/src/Controller/ArticleController.php
+++ the_spacebar/src/Controller/ArticleController.php
@@ -6,10 +6,21 @@ namespace App\Controller;
 
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
-class ArticleControler
+class ArticleController
 {
+    /**
+     * @Route("/")
+     */
     public function homepage() {
         return new Response("OMG my first page");
+    }
+
+    /**
+     * @Route("/news/{slug}")
+     */
+    public function show($slug) {
+        return new Response(sprintf("Future page for show page : %s", $slug));
     }
 }
