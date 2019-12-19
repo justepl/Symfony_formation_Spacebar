cb90354f1d793d19c93d37f8fbfd483e13bdf37d
Lamatiere <patrick.lamatiere@gmial.com>
2019-12-19 12:22:25 +0100

    finish the first symfony course

--- the_spacebar/src/Controller/ArticleController.php
+++ the_spacebar/src/Controller/ArticleController.php
@@ -6,20 +6,21 @@ namespace App\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="app_homepage")
      */
     public function homepage() {
-        return new Response("OMG my first page");
+        return $this->render('article/homepage.html.twig');
     }
 
     /**
-     * @Route("/news/{slug}")
+     * @Route("/news/{slug}", name="article_show")
      */
     public function show($slug) {
         $comments = [
@@ -30,7 +31,18 @@ class ArticleController extends AbstractController
 
         return $this->render('article/show.html.twig', [
             'title' => ucwords(str_replace('-', ' ', $slug)),
-            'comments' => $comments
+            'slug' => $slug,
+            'comments' => $comments,
         ]);
+    }
+
+    /**
+     * @Route("/news/{slug}/heart", name="article_toggle_heart",
+     *     methods={"POST"})
+     */
+    public function toggleArticleHeart($slug) {
+        //TODO = actually heart/unheart the article
+
+        return new JsonResponse(['hearts' => rand(5, 100)]);
     }
 }
